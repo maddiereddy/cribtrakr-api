f18c5708e9c44221002b1ae329ebf6714e3eb2a6
maddiereddy <maddiereddy@gmail.com>
2018-07-15 19:32:35 -0700

    modify server file

--- server.js
+++ server.js
@@ -27,12 +27,12 @@ const app = express();
 // Logging
 app.use(morgan('common'));
 
-// CORS
-app.use(
-  cors({
-    origin: CLIENT_ORIGIN
-  })
-);
+// // CORS
+// app.use(
+//   cors({
+//     origin: CLIENT_ORIGIN
+//   })
+// );
 
 app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
