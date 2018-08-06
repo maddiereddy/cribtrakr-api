7f7a50312af8d2011108a99519ddc71aac8dbe3a
maddiereddy <maddiereddy@gmail.com>
2018-08-06 15:35:09 -0700

    undo config changes

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 'use strict';
-exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://demo:demo1234@ds229701.mlab.com:29701/cribtrakr'; //'mongodb://localhost/cribtrakr';
-exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://demo:demo1234@ds229701.mlab.com:29701/cribtrakr-test'; //'mongodb://localhost/cribtrakr-test';
+exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/cribtrakr';
+exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/cribtrakr-test';
 exports.PORT = process.env.PORT || 8080;
 exports.CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || 'http://localhost:3000';
 exports.JWT_SECRET = process.env.JWT_SECRET || 'budapest';
